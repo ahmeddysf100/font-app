ad0cdf021474627177dca4dde8955534cd7de642
ahmeddysf100 <ahmedysf100@gmail.com>
2025-03-21 01:15:41 +0300

    Add optimization configuration to exclude 'crypto' from dependencies in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/font-app/',
+  optimizeDeps: {
+    exclude: ['crypto']
+  },
   plugins: [vue()],
   resolve: {
     alias: {
